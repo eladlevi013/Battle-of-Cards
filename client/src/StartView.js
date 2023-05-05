4216f0d6dbe4a9eb6a08122f1f56089abdb72a58
Elad Levi <60574244+eladlevi013@users.noreply.github.com>
2023-05-05 17:52:14 +0300

    not working routes

---
+++ client/src/StartView.js
@@ -0,0 +1,42 @@
+    import React, { useState } from 'react';
+    import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+    import 'bootstrap/dist/css/bootstrap.min.css';
+    import { useHistory } from 'react-router-dom';
+
+    function StartView(props) {
+        const [username, setUsername] = useState('');
+
+        function handleUsernameChange(event) {
+            setUsername(event.target.value);
+        }
+
+        function handleStartButtonClick() {
+            const history = useHistory();
+            history.push('/game');
+        }
+
+        return (
+            <Container style={{paddingTop: '100px'}} className="d-flex justify-content-center align-items-center h-100">
+            <Row>
+                <Col xs={12} md={12} className="mx-auto">
+                <h1 style={{color: 'white'}} className="text-center mb-4">Welcome To The Game</h1>
+                <Form>
+                    <Form.Group controlId="usernameInput">
+                    <Form.Label style={{color: 'white'}}>Choose a username:</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={username}
+                        onChange={handleUsernameChange}
+                    />
+                    </Form.Group>
+                    <Button style={{width:'100%', marginTop: '10px'}} variant="primary" onClick={handleStartButtonClick}>
+                    Start
+                    </Button>
+                </Form>
+                </Col>
+            </Row>
+            </Container>
+        );
+    }
+
+    export default StartView;
